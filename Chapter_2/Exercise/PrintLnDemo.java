6adcfcdd158ecaee2c303df24dca97355adb5be1
mohsinwarind <mohsinramzan999@gmail.com>
2024-08-06 20:51:25 +0500

    Adding More Programs

--- Chapter_2/Exercise/PrintLnDemo.java
+++ Chapter_2/Exercise/PrintLnDemo.java
@@ -2,8 +2,8 @@ package Exercise;
 
 public class PrintLnDemo {
     public static void main(String[] args) {
-        System.out.println("KFUEIT");
-        System.out.println("Rahim Yar Khan ");
-        System.out.println("JAVA");
+        System.out.println("Java");
+        System.out.println("Python ");
+        System.out.println("C++");
     }
 }
